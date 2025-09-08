3036ed195df64184a40fdb0fc58657cbc028e9dd
Ankush <ankkussshhh@gmail.com>
2025-09-09 00:03:27 +0530

    OOPS in Java

---
+++ src/Unit3/Vector1.java
@@ -0,0 +1,15 @@
+package Unit3;
+import java.util.Vector;
+public class Vector1 {
+    public static void main(String[] args) {
+        Vector<String> vt= new Vector<>(5,5);
+        vt.add("First");
+        vt.add("Second");
+        vt.add(2,"Forth");
+        System.out.println(vt.capacity());
+        System.out.println(vt.size());
+        System.out.println(vt.remove(0));
+        System.out.println(vt);
+
+    }
+}
