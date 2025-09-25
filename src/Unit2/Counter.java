032de3195cb587162883c108c50fe83cd1df645f
Ankush <ankkussshhh@gmail.com>
2025-09-25 10:42:33 +0530

    OOPS in Java

---
+++ src/Unit2/Counter.java
@@ -0,0 +1,12 @@
+package Unit2;
+
+public class Counter {
+    private int count = 0;
+    public  void increment() {
+        count++;
+
+    }
+    public int getCount() {
+        return count;
+    }
+}
